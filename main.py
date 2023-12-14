8b24a023d8a32625cf6bc795e4948ae8e8f96c65
Tan Dung <tandung12072002@gmail.com>
2023-12-14 11:07:49 +0700

    app

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, jsonify
+import os
+from werkzeug.utils import secure_filename
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def upload():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return jsonify({'error': 'No file part'}), 400
+        
+        f = request.files['file']
+        
+        if f.filename == '':
+            return jsonify({'error': 'No selected file'}), 400
+
+        file_path = secure_filename(f.filename)
+        f.save(file_path)
+
+
+        os.remove(file_path)
+
+
+        return jsonify({'message': 'File uploaded successfully'}), 200
+
+    # Trả về phản hồi nếu không phải là phương thức POST
+    return jsonify({'error': 'Method not allowed'}), 405
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/js/main.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+
+    $('.image-section').hide();
+    $('.loader').hide();
+    $('#result').hide();
+
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
+                $('#imagePreview').hide();
+                $('#imagePreview').fadeIn(650);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $("#imageUpload").change(function () {
+        $('.image-section').show();
+        $('#btn-predict').show();
+        $('#response').show();
+        $('#start').hide();
+        $('#result').text('');
+        $('#result').hide();
+        readURL(this);
+    });
+
+    // Predict
+    $('#btn-predict').click(function () {
+        var form_data = new FormData($('#upload-file')[0]);
+        console.log(form_data);
+
+        // Show loading animation
+        $(this).hide();
+        $('.loader').show();
+
+        $.ajax({
+            type: 'POST',
+            url: '/predict', 
+            data: form_data,
+            contentType: false,
+            cache: false,
+            processData: false,
+            async: true,
+            success: function (data) {
+                $('.loader').hide();
+                $('#result').fadeIn(600);
+                // $('#result').text(' Result:  ' + data);
+                console.log('Success!');
+            },
+            error: function (xhr, status, error) {
+                console.error('Error:', error);
+            },
+        });
+    });
+
+});
