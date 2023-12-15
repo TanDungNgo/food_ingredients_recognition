f157d8b2cf053b15e09ddcc8b8f70c6bf0d43633
ductran <93823088+tranhongduc@users.noreply.github.com>
2023-12-15 11:32:27 +0700

    design

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,8 @@
-$(document).ready(function () {
 
+
+
+
+$(document).ready(function () {
     $('.image-section').hide();
     $('.loader').hide();
     $('#result').hide();
@@ -25,6 +28,32 @@ $(document).ready(function () {
         readURL(this);
     });
 
+    function createRecipe(item) {
+        const image = document.createElement('img');
+        image.src = item.image;
+        image.alt = item.title;
+        const title = document.createElement('p');
+        title.textContent = item.title;
+        const container = document.createElement('div');
+        container.classList.add('recipe-container');
+        container.appendChild(image);
+        container.appendChild(title);
+        return container;
+    }
+
+    function createRecipeDetails(item) {
+        const image = document.createElement('img');
+        image.src = item.image;
+        image.alt = item.title;
+        const title = document.createElement('p');
+        title.textContent = item.title;
+        const container = document.createElement('div');
+        container.classList.add('recipe-container-detaile');
+        container.appendChild(image);
+        container.appendChild(title);
+        return container;
+    }
+
     // Predict
     $('#btn-predict').click(function () {
         var form_data = new FormData($('#upload-file')[0]);
@@ -46,6 +75,96 @@ $(document).ready(function () {
                 $('.loader').hide();
                 $('#result').fadeIn(600);
                 $('#result').text(' Result:  ' + data.result);
+
+
+                var dataDisplay = document.getElementById('dataDisplay');
+                dataDisplay.innerHTML = '';
+
+                var resultDisplay = document.getElementById('resultDisplay');
+                if (resultDisplay) {
+
+                    var newElement = document.createElement('div');
+                    resultDisplay.appendChild(newElement);
+                    resultDisplay.innerHTML = ''; 
+                    // Các thao tác khác trên resultDisplay
+                } else {
+                    console.error('Không tìm thấy phần tử resultDisplay.');
+                }
+                
+
+
+                for (const key in data.data.results) {
+                    if (data.data.results.hasOwnProperty(key)) {
+                        const item = data.data.results[key];
+
+                        const container = createRecipe(item);
+                        dataDisplay.appendChild(container);
+
+                        container.addEventListener('click', function () {
+
+                            console.log('Clicked:', item);
+
+                            $('#dataDisplay').hide();
+
+
+                            
+                            while (resultDisplay.firstChild) {
+                                resultDisplay.removeChild(resultDisplay.firstChild);
+                            }
+                            // Tạo phần tử mới từ createRecipeDetails(item) và thêm vào resultDisplay
+                            var resultDetail = createRecipeDetails(item);
+                            resultDisplay.appendChild(resultDetail);
+                        });
+                    }
+                }
+
+                // var dataDisplay = document.getElementById('dataDisplay');
+                // dataDisplay.innerHTML = ''; 
+                //     for (const key in data.data.results) {
+                //         if (data.data.results.hasOwnProperty(key)) {
+                //             const item = data.data.results[key];
+
+
+                //             const image = document.createElement('img');
+                //             image.src = item.image;
+                //             image.alt = item.title;
+                    
+
+                //             const title = document.createElement('p');
+                //             title.textContent = item.title;
+                //             const container = document.createElement('div');
+
+                //             container.classList.add('recipe-container');
+
+                //             container.appendChild(image);
+                //             container.appendChild(title);
+                    
+                //             dataDisplay.appendChild(container);
+
+                //             container.addEventListener('click', function () {
+                //                 console.log('Clicked:', item);
+
+                //                 var resultDisplay = document.getElementById('resultDisplay');
+                //                 resultDisplay.innerHTML = ''; 
+        
+
+                //                 const image = document.createElement('img');
+                //                 image.src = item.image;
+                //                 image.alt = item.title;
+                                    
+
+                //                 const title = document.createElement('p');
+                //                 title.textContent = item.title;
+                //                 const container = document.createElement('div');
+
+                //                 container.classList.add('recipe-container');
+                //                 container.appendChild(image);
+                //                 container.appendChild(title);
+                                    
+                //                 resultDisplay.appendChild(container);                           
+                //             });
+                //         }
+                //     }
 
                 console.log('Success!');
                 console.log("Data:", data.data)
